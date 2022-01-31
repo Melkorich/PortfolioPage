58f83e3e97e2bcad30f14021aacc23622d9a369c
Melkorich <Kirill.Lavrov92@gmail.com>
2022-01-31 19:16:58 +0300

    preloader added

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
 $(function(){
+
+  //show more btn
+  $('.portfolio__item').slice(0, 6).show();
+  $('.portfolio__btn').on('click', function() {
+    $('.portfolio__item:hidden').slice(0, 3).slideDown();
+
+    if ($('.portfolio__item:hidden').length == 0) {
+      $('.portfolio__btn').fadeOut('slow');
+    }
+  })
+
+  //smooth scroll
   $("a[href^='#']").click(function(){
           var _href = $(this).attr("href");
           $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 1000);
           return false;
   });
 });
+
+//preloader
+const loader = document.getElementById('preloader');
+
+window.addEventListener('load', function() {
+  loader.style.display = 'none';
+})
